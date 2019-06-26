87c45cf87e2bbfd3e68d96043a54edff0f28fb61
nandakumarpurohit <nandakumarpurohit@gmail.com>
2019-06-26 15:22:21 -0400

    init files

---
+++ emptag/emp.sql
@@ -0,0 +1,14 @@
+create table emp(
+	id int,
+		fname varchar(20),
+		lname varchar(20),
+		age int,
+		salary double
+		);
+insert into emp values(25, 'John', 'Smith', 30, 85000);
+insert into emp values(10, 'John', 'Rambo', 25, 75000);
+insert into emp values(40, 'James', 'Bond', 35, 10000);
+insert into emp values(28, 'James', 'Ghosling', 18, 125000);
+insert into emp values(22, 'Will', 'Smith', 40, 150000);
+
+commit;
